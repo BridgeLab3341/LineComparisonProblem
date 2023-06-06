6e597508ddb53b45bb561655a8fd59ed4a51a3dd
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-06 19:22:03 +0530

    [somu]Add:Calculating Length of Line.

--- LineComparisionProblem/LineOperation.cs
+++ LineComparisionProblem/LineOperation.cs
@@ -18,7 +18,7 @@ namespace LineComparisionProblem
             double y1=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter y2 values");
             double y2=Convert.ToInt32(Console.ReadLine());
-            double length=Math.Sqrt(Math.Pow((x2-x1),2+Math.Pow((y2-y1),2)));
+            double length=Math.Sqrt((Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)));
             Console.WriteLine("Length of Line is : {0}",length);
         }
     }
