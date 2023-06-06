2b706475b494c3885c6861147769d913b831478c
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-06 13:31:28 +0530

    [somu]Add:Calculating Length of Line.

---
+++ LineComparisionProblem/LineOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    public class LineOPeration
+    {
+        public void CalculateLength()
+        {
+            Console.WriteLine("Enter x1 values");
+            double x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2 values");
+            double x2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1 values");
+            double y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2 values");
+            double y2=Convert.ToInt32(Console.ReadLine());
+            double length=Math.Sqrt(Math.Pow((x2-x1),2+Math.Pow((y2-y1),2)));
+            Console.WriteLine("Length of Line is : {0}",length);
+        }
+    }
+}
---
+++ LineComparisionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LineOPeration lineOPeration = new LineOPeration();
+            lineOPeration.CalculateLength();
+        }
+    }
+}
